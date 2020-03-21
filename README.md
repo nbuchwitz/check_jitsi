88ce359879e169ec67ca7130373946956ccbf38f
Nicolai Buchwitz <nicolai.buchwitz@enda.eu>
2020-03-21 19:37:32 +0100

    first version

---
+++ README.md
@@ -0,0 +1,112 @@
+# check_jitsi
+Icinga check command for jitsi video conference server
+
+## Setup
+
+### Requirements
+
+This check command depends on **Python 3** and the following modules:
+ * enum
+ * requests
+ * argparse
+
+**Installation on Debian / Ubuntu**
+```
+apt install python3 python3-requests
+```
+
+**Installation on Redhat 7 / CentOS 7**
+```
+yum install python36 python36-requests
+```
+
+**Installation on FreeBSD**
+```
+pkg install python3 py37-requests
+```
+
+
+### Activate private rest API on jitsi videobridge
+
+Enable rest api in jitsi videobridge by appending `--apis=rest` to the `JVB_OPTS` in `/etc/jitsi/videobridge/config`:
+
+```
+[...]
+JVB_OPTS="--apis=rest,xmpp"
+[...]
+```
+
+Edit `/etc/jitsi/videobridge/sip-communicator.properties` and enable statistics:
+
+```
+[...]
+org.jitsi.videobridge.ENABLE_STATISTICS=true
+[...]
+```
+
+Restart the `jvb` and `jicofo` service
+
+#### Troubleshooting
+
+If using openjdk and you see lots of errors in your videobridge logs, try to enable `jdk.management/com.sun.management`, by editing `/etc/jitsi/videobridge/config`:
+
+before:
+```
+JAVA_SYS_PROPS="-Dnet.java.sip.communicator.SC_HOME_DIR_LOCATION=/etc/jitsi -Dnet.java.sip.communicator.SC_HOME_DIR_NAME=videobridge -Dnet.java.sip.communicator.SC_LOG_DIR_LOCATION=/var/log/jitsi -Djava.util.logging.config.file=/etc/jitsi/videobridge/logging.properties"
+```
+
+after:
+```
+JAVA_SYS_PROPS="-Dnet.java.sip.communicator.SC_HOME_DIR_LOCATION=/etc/jitsi -Dnet.java.sip.communicator.SC_HOME_DIR_NAME=videobridge -Dnet.java.sip.communicator.SC_LOG_DIR_LOCATION=/var/log/jitsi -Djava.util.logging.config.file=/etc/jitsi/videobridge/logging.properties --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED"
+```
+
+## Usage
+
+```
+usage: check_jitsi.py [-h] [-H HOSTNAME] [-p PORT] -m
+                      {health,participants,conferences,audiochannels,videochannels,videostreams,total_conferences_completed,total_conferences_created,total_conferences_failed,total_partially_failed_conferences,jitter_aggregate,total_no_payload_channels,total_no_transport_channels}
+                      [-w THRESHOLD] [-c THRESHOLD] [--all-metrics]
+                      [--ignore-metric METRIC]
+
+Check command for JVB via API
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -H HOSTNAME, --hostname HOSTNAME
+                        JVB private api hostname
+  -p PORT, --port PORT  JVB private api port
+  -m {health,participants,conferences,audiochannels,videochannels,videostreams,total_conferences_completed,total_conferences_created,total_conferences_failed,total_partially_failed_conferences,jitter_aggregate,total_no_payload_channels,total_no_transport_channels}, --mode {health,participants,conferences,audiochannels,videochannels,videostreams,total_conference
+s_completed,total_conferences_created,total_conferences_failed,total_partially_failed_conferences,jitter_aggregate,total_no_payload_channels,total_no_transport_channels}
+                        Check mode
+  -w THRESHOLD, --warning THRESHOLD
+                        Warning threshold for check value
+  -c THRESHOLD, --critical THRESHOLD
+                        Critical threshold for check value
+  --all-metrics
+  --ignore-metric METRIC
+                        Ignore this metric in the performance data
+```
+
+## Examples
+
+**Check jitsi health**
+```
+./check_pve.py -m health
+OK - Jitsi videobridge is healthy 
+```
+
+with all metrics:
+
+```
+./check_jitsi.py -m health --all-metrics 
+OK - Jitsi videobridge is healthy |packet_rate_download=0 total_tcp_connections=0 total_packets_sent_octo=0 total_loss_degraded_participant_seconds=0 bit_rate_download=0 videostreams=0 jitter_aggregate=0.0 total_channels=8 total_memory=16821 total_packets_received=13694 rtt_aggregate=0.0 packet_rate_upload=0 conferences=0 participants=0 total_loss_limited_participant_seconds=0 largest_conference=0 total_packets_sent=3030 total_data_channel_messages_sent=609 total_bytes_received_octo=0 total_no_transport_channels=0 total_no_payload_channels=4 used_memory=1518 total_conferences_created=2 threads=82 total_colibri_web_socket_messages_received=0 videochannels=0 total_udp_connections=4 loss_rate_upload=0.0 total_packets_received_octo=0 graceful_shutdown=0 total_colibri_web_socket_messages_sent=0 total_bytes_sent_octo=0 total_data_channel_messages_received=633 loss_rate_download=0.0 total_conference_seconds=802 total_bytes_received=9662349 rtp_loss=0.0 total_loss_controlled_participant_seconds=0 total_partially_failed_conferences=2 bit_rate_upload=0 total_conferences_completed=2 total_bytes_sent=99326 total_failed_conferences=0 cpu_usage=0.005 audiochannels=0
+```
+
+**Check specific metric**
+
+It's possible to check if a certain metric is within the specified thresholds for warning and critical (full syntax supported):
+```
+./check_jitsi.py -m total_conferences_failed -w 5 -c 10
+OK - 0 total_conferences_failed |total_conferences_failed=0;;5;10
+
+```
---
+++ check_jitsi.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim: ts=4:sw=4:et
+
+# ------------------------------------------------------------------------------
+# check_nextcloud.py - A check plugin for Jitsi.
+# Copyright (C) 2018-2020  Nicolai Buchwitz <nb@tipi-net.de>
+#
+# Version: 1.0.0
+#
+# ------------------------------------------------------------------------------
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# ------------------------------------------------------------------------------
+
+
+import sys
+import re
+import requests
+import argparse
+from enum import Enum
+
+
+class CheckState(Enum):
+    OK = 0
+    WARNING = 1
+    CRITICAL = 2
+    UNKNOWN = 3
+
+
+# adapted from https://github.com/nlm/nagplug/blob/master/nagplug/__init__.py
+class Threshold(object):
+
+    def __init__(self, threshold):
+        self._threshold = threshold
+        self._min = 0
+        self._max = 0
+        self._inclusive = False
+        self._parse(threshold)
+
+    def _parse(self, threshold):
+        match = re.search(r'^(@?)((~|\d*):)?(\d*)$', threshold)
+
+        if not match:
+            raise ValueError('Error parsing Threshold: {0}'.format(threshold))
+
+        if match.group(1) == '@':
+            self._inclusive = True
+
+        if match.group(3) == '~':
+            self._min = float('-inf')
+        elif match.group(3):
+            self._min = float(match.group(3))
+        else:
+            self._min = float(0)
+
+        if match.group(4):
+            self._max = float(match.group(4))
+        else:
+            self._max = float('inf')
+
+        if self._max < self._min:
+            raise ValueError('max must be superior to min')
+
+    def check(self, value):
+        if self._inclusive:
+            return True if self._min <= value <= self._max else False
+        else:
+            return True if value > self._max or value < self._min else False
+
+    def __repr__(self):
+        return '{0}({1})'.format(self.__class__.__name__, self._threshold)
+
+    def __str__(self):
+        return self._threshold
+
+
+class CheckJitsi:
+    VERSION = '1.0.0'
+
+    def __init__(self):
+        self.args = {}
+        self._statistics = None
+
+        self._simple_modes = ['participants', 'conferences', 'audiochannels', 'videochannels', 'videostreams',
+                              'total_conferences_completed', 'total_conferences_created', 'total_conferences_failed',
+                              'total_partially_failed_conferences',
+                              'jitter_aggregate', 'total_no_payload_channels', 'total_no_transport_channels']
+
+        self.parse_args()
+        self._baseurl = f"http://{self.args.hostname}:{self.args.port}"
+
+        self._state = CheckState.OK
+
+        self._warning = Threshold(self.args.threshold_warning)
+        self._criticial = Threshold(self.args.threshold_critical)
+
+    def parse_args(self):
+        p = argparse.ArgumentParser(description='Check command for JVB via API')
+
+        p.add_argument("-H", "--hostname", required=False, help="JVB private api hostname", default='localhost')
+        p.add_argument("-p", "--port", required=False, default=8080, help="JVB private api port")
+
+        p.add_argument("-m", "--mode",
+                       choices=['health'] + self._simple_modes,
+                       required=True,
+                       help="Check mode")
+        p.add_argument('-w', '--warning', dest='threshold_warning', metavar='THRESHOLD',
+                       help='Warning threshold for check value', default="")
+        p.add_argument('-c', '--critical', dest='threshold_critical', metavar='THRESHOLD',
+                       help='Critical threshold for check value', default="")
+        p.add_argument("--all-metrics", action='store_true', required=False)
+        p.add_argument("--ignore-metric", dest='metric_blacklist', metavar='METRIC', action='append', default=[],
+                       required=False,
+                       help='Ignore this metric in the performance data')
+
+        self.args = p.parse_args()
+
+    @property
+    def statistics(self):
+        if self._statistics is None:
+            r = self._fetch('/colibri/stats')
+
+            if r.status_code != 200:
+                # TODO: handle?
+                raise Exception("Could not fetch colibri stats")
+
+            # see https://github.com/jitsi/jitsi-videobridge/blob/master/doc/statistics.md
+            self._statistics = r.json()
+
+        # TODO: more filter?
+        for k in ['conference_sizes', 'current_timestamp'] + self.args.metric_blacklist:
+            if k in self._statistics:
+                del (self._statistics[k])
+
+        return self._statistics
+
+    def check_result(self, rc, message, metrics=None):
+        prefix = rc.name
+        message = '{} - {}'.format(prefix, message)
+        perfdata = ""
+
+        if metrics is not None and self.args.all_metrics:
+            metrics.update(self.statistics)
+
+        if metrics:
+            perfdata = '|'
+            perfdata += ' '.join([f"{k}={int(v)}" if type(v) == bool else f"{k}={v}" for k, v in metrics.items()])
+
+        print(message, perfdata)
+        sys.exit(rc.value)
+
+    def _fetch(self, uri):
+        try:
+            response = requests.get(
+                f"{self._baseurl}{uri}",
+            )
+        except requests.exceptions.ConnectTimeout:
+            self.check_result(CheckState.UNKNOWN, "Could not connect to JVB API: Connection timeout")
+        except requests.exceptions.SSLError:
+            self.check_result(CheckState.UNKNOWN, "Could not connect to JVB API: Certificate validation failed")
+        except requests.exceptions.ConnectionError:
+            self.check_result(CheckState.UNKNOWN,
+                              "Could not connect to JVB API: Failed to resolve hostname or service is not listenings")
+
+        return response
+
+    def check_health(self):
+        r = self._fetch('/about/health')
+        healthy = r.status_code == 200
+        msg = "Jitsi videobridge is healthy"
+
+        if not healthy:
+            msg = f"Jitsi videobridge health check failed ({r.status_code})"
+            self._state = CheckState.CRITICAL
+
+        self.check_result(self._state, msg, {})
+
+    def _check_simple(self, name):
+        value = self.statistics.get(name, 0)
+        msg = f"{value} {name}"
+
+        if self._criticial.check(value):
+            self._state = CheckState.CRITICAL
+        elif self._warning.check(value):
+            self._state = CheckState.WARNING
+
+        self.check_result(self._state, msg,
+                          {name: f"{value};;{self.args.threshold_warning};{self.args.threshold_critical}"})
+
+    def check(self):
+        if self.args.mode == 'health':
+            self.check_health()
+        if self.args.mode in self._simple_modes:
+            self._check_simple(self.args.mode)
+
+
+nc = CheckJitsi()
+nc.check()
